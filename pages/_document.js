5d91158729d16c95068245f743daf538c1d51c99
Michal <m@Michals-MacBook-Pro.local>
2024-07-24 15:15:15 +0200

    first commit18

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16648022210"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16648022210');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

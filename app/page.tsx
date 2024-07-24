83598f94ea39b5d817da2ea8ef3e679ae2af2ef9
Michal <m@michals-mbp.home>
2024-07-24 16:07:44 +0200

    first commit25

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
 export default function HeroSection() {
   return (
     <div className="">
-      <div className="h-full w-full bg-gradient-to-br from-blue-400 to-cyan-100 bg-cover bg-center py-4">
+      <div className="h-full w-full bg-gradient-to-b  bg-gradient-to-r from-blue-800 to-indigo-900 bg-cover bg-center py-4">
         {/* <div className="mx-auto h-8 w-fit">
           <a href="#" className="flex items-center">
             <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -17,11 +17,11 @@ export default function HeroSection() {
           </a>
           <br />
         </div> */}
-        <div className="relative z-10 h-96 h-full justify-center text-center text-black">
-          <p className="text-md mb-2 text-zinc-900">
+        <div className="relative z-10 h-96 h-full justify-center text-center text-white">
+          <p className="text-md mb-2 ">
             Complete two <span className="underline">free steps</span> to unlock*
           </p>
-          <h1 className="px-4 text-2xl font-bold leading-7 text-zinc-900">Up to 62,500 V-Bucks Worth Of Reward Access^</h1>
+          <h1 className="px-4 text-2xl font-bold leading-7 ">Up to 62,500 V-Bucks Worth Of Reward Access^</h1>
           {/* <div className="flex gap-2 overflow-hidden">
             <Image src="/fortnite-2800.jpg" className="fit mx-auto my-2 w-36 rounded-lg shadow-xl" alt="reward" width={673} height={973}></Image>
           </div> */}
@@ -37,7 +37,7 @@ export default function HeroSection() {
           {/* <Link href="vbucks/access" className="px-6  block mx-4 py-4 text-2xl font-semibold rounded-full text-white bg-blue-600">
             Get Started!{" "}
           </Link> */}
-          <div className="px-10 text-center font-sans text-xs leading-4 text-zinc-600">
+          <div className="px-10 text-center font-sans text-xs leading-4 text-zinc-100">
             <p>
               Trade names or rights associated with all brands on this site are the property of their respective owners and are not affiliated with this promotion. <br />
               <strong>*Upon completion two steps such as reaching certain level in free games.</strong> ^Receive access to $500 Gift Card promotions.
